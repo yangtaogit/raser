0f99b36b2a7cafe6b7b5f00ba21c0252f948570b
zylyz18 <yzlee626@gmail.com>
2021-09-14 09:46:41 +0800

    test

--- raser/__init__.py
+++ raser/__init__.py
@@ -1,8 +1,8 @@
 from sys import modules
-from raser.geometry import R3dDetector
-from raser.pyfenics import FenicsCal
-from raser.g4particles import Particles
-from raser.calcurrent import CalCurrent
+#from raser.geometry import R3dDetector
+#from raser.pyfenics import FenicsCal
+#from raser.g4particles import Particles
+#from raser.calcurrent import CalCurrent
 from raser.elereadout import Amplifier
 from raser.setting import Setting
 
